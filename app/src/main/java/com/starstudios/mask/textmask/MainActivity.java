3e6733f3461d14ba764299de01b81aa39d186838
Saurabh Singh <saurabhsinghmay99@gmail.com>
2018-05-18 17:03:55 +0530

    Adding gyroscope function

--- app/src/main/java/com/starstudios/mask/textmask/MainActivity.java
+++ app/src/main/java/com/starstudios/mask/textmask/MainActivity.java
@@ -7,13 +7,38 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gjiazhe.panoramaimageview.GyroscopeObserver;
+import com.gjiazhe.panoramaimageview.PanoramaImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GyroscopeObserver gyroscopeObserver;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gyroscopeObserver = new GyroscopeObserver();
+        gyroscopeObserver.setMaxRotateRadian(Math.PI/9);
+
+        PanoramaImageView panoramaImageView = (PanoramaImageView) findViewById(R.id.panorama_image_view);
+        panoramaImageView.setGyroscopeObserver(gyroscopeObserver);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register GyroscopeObserver.
+        gyroscopeObserver.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister GyroscopeObserver.
+        gyroscopeObserver.unregister();
     }
 }
