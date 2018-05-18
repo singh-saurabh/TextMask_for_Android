479cb73e70c973eb2986dbd0888c5ca04c7e52a0
Saurabh Singh <saurabhsinghmay99@gmail.com>
2018-05-18 16:15:36 +0530

    Initial commit

---
+++ app/src/main/java/com/starstudios/mask/textmask/MainActivity.java
@@ -0,0 +1,19 @@
+package com.starstudios.mask.textmask;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
