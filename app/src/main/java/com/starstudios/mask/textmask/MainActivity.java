a589115e53f22077fb05f2f639d6bde74dfbf02b
Saurabh Singh <saurabhsinghmay99@gmail.com>
2018-05-19 10:36:40 +0530

    Final touches

--- app/src/main/java/com/starstudios/mask/textmask/MainActivity.java
+++ app/src/main/java/com/starstudios/mask/textmask/MainActivity.java
@@ -1,44 +1,57 @@
 package com.starstudios.mask.textmask;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorListener;
-import android.hardware.SensorManager;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gjiazhe.panoramaimageview.GyroscopeObserver;
 import com.gjiazhe.panoramaimageview.PanoramaImageView;
 
+import it.gilvegliach.android.transparenttexttextview.TransparentTextTextView;
+
 public class MainActivity extends AppCompatActivity {
 
     private GyroscopeObserver gyroscopeObserver;
-    private TextView textView;
+    private PanoramaImageView panoramaImageView;
+    private TransparentTextTextView textView;
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gyroscopeObserver = new GyroscopeObserver();
-        gyroscopeObserver.setMaxRotateRadian(Math.PI/9);
+        setViews();
 
-        PanoramaImageView panoramaImageView = (PanoramaImageView) findViewById(R.id.panorama_image_view);
+
+    }
+    void setViews(){
+        textView = findViewById(R.id.textView) ;
+        Typeface font= Typeface.createFromAsset(getAssets(), "fonts/Raleway-ExtraBold.ttf");
+        textView.setTypeface(font);
+
+        textView2 = findViewById(R.id.textView4);
+        Typeface font2= Typeface.createFromAsset(getAssets(), "fonts/Raleway-Thin.ttf");
+        textView2.setTypeface(font2);
+
+        gyroscopeObserver = new GyroscopeObserver();
+        gyroscopeObserver.setMaxRotateRadian(Math.PI/2);
+
+        panoramaImageView = findViewById(R.id.panorama_image_view);
         panoramaImageView.setGyroscopeObserver(gyroscopeObserver);
 
     }
+
+
     @Override
     protected void onResume() {
         super.onResume();
-        // Register GyroscopeObserver.
         gyroscopeObserver.register(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        // Unregister GyroscopeObserver.
         gyroscopeObserver.unregister();
     }
 }
